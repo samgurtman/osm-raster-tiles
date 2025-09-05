59ceeedcb3659895800c43f167012c4345c8a20c
Sam Gurtman <sam@gurtman.com>
2025-09-05 10:59:36 -0500

    More tweaks

--- generate.sh
+++ generate.sh
@@ -1,8 +1,11 @@
-sudo yum install -y curl java-21-amazon-corretto python3
+sudo yum install -y curl java-21-amazon-corretto python3 git
+
+
 curl -fsSL https://rpm.nodesource.com/setup_22.x | sudo bash -
 sudo yum install -y nodejs
 
-
+git clone https://github.com/samgurtman/osm-raster-tiles.git /opt/osm-raster-tiles
+cd /opt/osm-raster-tiles
 
 mkdir data
 curl --fail -L https://download.geofabrik.de/north-america/us-latest.osm.pbf -o data/us-latest.osm.pbf
@@ -10,9 +13,8 @@ curl --fail -L https://github.com/onthegomap/planetiler/releases/download/v0.9.1
 java -Xmx12g -jar planetiler.jar --osm-path=$(pwd)/data/us-latest.osm.pbf --download --output data/us-vector.mbtiles
 npm install -g tileserver-gl
 tileserver-gl --silent &
-. .venv/bin/activate
-python3 -m pip install
+python3 -m pip install -r requirements.txt
 python3 generate.py -w 8 -s 256
 output="$(date +"%Y-%m-%d").mbtiles"
 mb-util tiles-256 "$output"
-aws s3 cp large_test_file "s3://osm-freflight-raster/$output"
+aws s3 cp "$output" "s3://osm-freflight-raster/$output"
