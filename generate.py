9de9831720dc82c63c6f3197e2e3a77dc79f123d
Sam Gurtman <sam@gurtman.com>
2025-09-05 14:24:26 -0500

    updates

--- generate.py
+++ generate.py
@@ -16,14 +16,14 @@ def deg2num(lat_deg, lon_deg, zoom):
   
       
 def render(tuple):
-  output_dir, tile_size, zoom, x, y = tuple
+  temp_dir, output_dir, tile_size, zoom, x, y = tuple
   file_loc=f"{output_dir}/{zoom}/{x}/{y}.png"
   if os.path.isfile(file_loc):
     return
    
   os.makedirs(f"{output_dir}/{zoom}/{x}", exist_ok=True)
   url=f"http://localhost:8080/styles/foreflight/{tile_size}/{zoom}/{x}/{y}.png"
-  temp = tempfile.NamedTemporaryFile(delete=False, dir="data/temp")
+  temp = tempfile.NamedTemporaryFile(delete=False, dir=temp_dir)
   urllib.request.urlretrieve(url, temp.name)
   os.replace(temp.name, file_loc)
   return
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--tile-size', type=int, default=256, help='tile size in pixels')
     parser.add_argument('-o', '--output-dir', type=str, default="tiles", help='output dir')
     parser.add_argument('-t', '--temp-dir', type=str, default="temp-tiles", help='temp dir')
-
+    
 
     args = parser.parse_args()
 
@@ -47,7 +47,7 @@ if __name__ == '__main__':
       end_x, end_y = deg2num(24, -66, zoom)
       for x in range(start_x, end_x + 1):
         for y in range(start_y, end_y + 1):
-          tiles.append((args.output_dir, args.tile_size, zoom, x, y))
+          tiles.append((args.temp_dir, args.output_dir, args.tile_size, zoom, x, y))
 
     with Pool(args.workers) as p:
         for result in tqdm.tqdm(p.imap_unordered(render, tiles, 200), total = len(tiles)):
--- generate.sh
+++ generate.sh
@@ -1,22 +1,29 @@
 set -ex
-sudo yum install -y java-21-amazon-corretto python3 git
 
+trap "shutdown now" EXIT
 
+wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
+
+sudo apt-get update
+sudo apt-get install -y java-21-amazon-corretto-jdk libbz2-dev libjpeg-turbo8 libglx-dev python3 curl libopengl-dev
 curl -fsSL https://rpm.nodesource.com/setup_22.x | sudo bash -
-sudo yum install -y nodejs
+sudo apt-get install -y nodejs
 
 
 sudo mkfs -t xfs /dev/nvme1n1
 mkdir data
 sudo mount /dev/nvme1n1 data
-
-curl --fail -L https://download.geofabrik.de/north-america/us-latest.osm.pbf -o data/us-latest.osm.pbf
+sudo chown -R ubuntu data
+curl -L https://download.geofabrik.de/north-america/us-latest.osm.pbf -o data/us-latest.osm.pbf 
 curl --fail -L https://github.com/onthegomap/planetiler/releases/download/v0.9.1/planetiler.jar -o planetiler.jar
-java -Xmx12g -jar planetiler.jar --osm-path=$(pwd)/data/us-latest.osm.pbf --download --output data/us-vector.mbtiles
-npm install -g tileserver-gl
+
+java -Xmx32g -jar planetiler.jar --osm-path=$(pwd)/data/us-latest.osm.pbf --download --output data/us-vector.mbtiles
+sudo npm install -g tileserver-gl
 tileserver-gl --silent &
 python3 -m pip install -r requirements.txt
-python3 generate.py -w 8 -s 256 -t data/temp -o data/tiles
+python3 generate.py -w 8 -s 256 -t data/temp -o data/tiles -b "$bounds"
 output="data/$(date +"%Y-%m-%d").mbtiles"
 mb-util data/tiles "$output"
 aws s3 cp "$output" "s3://osm-freflight-raster/$output"
+
